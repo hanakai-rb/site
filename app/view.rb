e729d60384514f5bb65965731501ecd34bfcadca
Max Wheeler <max@makenosound.com>
2025-09-25 08:40:48 +1000

    Rough out the structure for dynamic nav

--- app/view.rb
+++ app/view.rb
@@ -12,5 +12,25 @@ module Site
 
     # Used in the app layout
     expose :settings, decorate: false
+
+    NavItem = Data.define(:label, :url, :selected, :children)
+    expose :nav_items, layout: true do
+      [
+        NavItem.new(label: "Guides", url: "/guides", selected: false, children: [
+          NavItem.new(label: "Hanami", url: "/guides/hanami/v2.2/getting-started", selected: false, children: []),
+          NavItem.new(label: "Dry", url: "/guides/dry/v1.0/getting-started", selected: false, children: []),
+          NavItem.new(label: "Rom", url: "/guides/rom/v5.0/getting-started", selected: false, children: [])
+        ]),
+        NavItem.new(label: "Docs", url: "/docs", selected: false, children: [
+          NavItem.new(label: "Hanami", url: "/docs/#hanami", selected: false, children: []),
+          NavItem.new(label: "Dry", url: "/docs/#dry", selected: false, children: []),
+          NavItem.new(label: "Rom", url: "/docs/#rom", selected: false, children: [])
+        ]),
+        NavItem.new(label: "Blog", url: "/blog", selected: false, children: []),
+        NavItem.new(label: "Community", url: "/blog", selected: false, children: []),
+        NavItem.new(label: "Conduct", url: "/conduct", selected: false, children: []),
+        NavItem.new(label: "Sponsor", url: "/sponsor", selected: false, children: [])
+      ]
+    end
   end
 end
