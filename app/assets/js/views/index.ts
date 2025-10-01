7f37bc77585efc2ea3f021456b770a5ab7310c07
Max Wheeler <max@makenosound.com>
2025-10-01 22:01:24 +1000

    Move around

--- app/assets/js/views/index.ts
+++ app/assets/js/views/index.ts
@@ -2,7 +2,7 @@ import type { Views } from "@icelab/defo";
 
 import { breakpointFilter } from "~/utils/breakpoints";
 import { lazyLoadView } from "~/utils/lazy-load";
-import { docsearchViewFn } from "./docsearch/docsearch";
+import { docsearchViewFn } from "./docsearch";
 import { sizeToVarViewFn } from "./size-to-var";
 import { tocScrollViewFn } from "./toc-scroll";
 import { toggleClassViewFn } from "./toggle-class";
