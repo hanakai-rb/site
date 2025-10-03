d101fe3b4a5f914a1deefd90e0fb84abd81d6635
Max Wheeler <max@makenosound.com>
2025-10-03 21:07:42 +1000

    Expose `theme` to layout and use to switch theme as you navigate between orgs (#102)
    Fairly naive implementation for now, but does bring up the question of
    whether we should add `org` as a more explicit param to the URLs under
    `/docs`. What do you reckon, @timriley?

--- app/view.rb
+++ app/view.rb
@@ -25,5 +25,33 @@ module Site
         NavItem.new(label: "Sponsor", url: "/sponsor", selected: path == "/sponsor", children: [])
       ]
     end
+
+    expose :theme, layout: true, decorate: false do |context:, org: nil, slug: nil|
+      orgs = %w[hanami dry rom]
+
+      detected = nil
+
+      # If org was provided explicitly (e.g. from guides actions), trust it
+      if org && orgs.include?(org)
+        detected = org
+      end
+
+      path = context.request.path
+
+      # Infer org from /docs/:slug pattern
+      if detected.nil?
+        slug ||= path[/\A\/docs\/([^\/]+)/, 1]
+        if slug
+          if orgs.include?(slug)
+            detected = slug
+          else
+            prefix = slug.split(/[-_]/).first
+            detected = prefix if orgs.include?(prefix)
+          end
+        end
+      end
+
+      detected || "hanakai"
+    end
   end
 end
