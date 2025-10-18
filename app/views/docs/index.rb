a302cb5ea276fc044ad225afc52e8d59f7ac9bd8
Philip Arndt <git@p.arndt.io>
2025-10-18 22:52:18 +1100

    Make the docs pages also look nicer

--- app/views/docs/index.rb
+++ app/views/docs/index.rb
@@ -9,6 +9,14 @@ module Site
         expose :docs do
           doc_repo.latest_by_org
         end
+
+        expose :versions do
+          doc_repo.all.group_by(&:org).transform_values do |org_docs|
+            org_docs.group_by(&:slug).transform_values do |slug_docs|
+              slug_docs.map(&:version).sort.reverse
+            end
+          end
+        end
       end
     end
   end
