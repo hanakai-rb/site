e36a6c553ba67ff6d629559101ab6f3041d9edfd
Josephine Hall <josephinehall@gmail.com>
2025-10-18 17:30:24 +1100

    Remove existing Algolia implementation

--- app/assets/js/views/index.ts
+++ app/assets/js/views/index.ts
@@ -2,7 +2,6 @@ import type { Views } from "@icelab/defo";
 
 import { breakpointFilter } from "~/utils/breakpoints";
 import { lazyLoadView } from "~/utils/lazy-load";
-import { docsearchViewFn } from "./docsearch";
 import { ensureActiveNavLinkVisibleViewFn } from "./ensure-active-nav-link-visible";
 import { sizeToVarViewFn } from "./size-to-var";
 import { staticSearchViewFn } from "./static-search";
@@ -10,7 +9,6 @@ import { tocScrollViewFn } from "./toc-scroll";
 import { toggleClassViewFn } from "./toggle-class";
 
 export const views: Views = {
-  docsearch: docsearchViewFn,
   ensureActiveNavLinkVisible: ensureActiveNavLinkVisibleViewFn,
   foresight: lazyLoadView(async () => {
     const { foresightViewFn } = await import("./foresight");
