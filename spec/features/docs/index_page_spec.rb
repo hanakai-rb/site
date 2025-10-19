1f762331ecb5876a7e3a64225c0ede203a429696
Philip Arndt <git@p.arndt.io>
2025-10-19 11:50:02 +1100

    Update specs to make sure that we're linking to everything that we're supposed to be linking to

--- spec/features/docs/index_page_spec.rb
+++ spec/features/docs/index_page_spec.rb
@@ -1,15 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.feature "Docs / Index page" do
+  let(:doc_repo) { Site::App["repos.doc_repo"] }
+
   it "lists all docs across orgs" do
     visit "/docs"
 
-    within "[data-testid=dry-docs]" do
-      expect(page.find_all("li a").map(&:text)[0..2]).to eq [
-        "dry-auto_inject",
-        "dry-equalizer",
-        "dry-cli"
-      ]
+    expected_docs = doc_repo.latest_by_org
+
+    expected_docs.each do |org, docs|
+      within "[data-testid=#{org}-docs]" do
+        doc_titles = page.find_all("a h3").map(&:text)
+        expect(doc_titles).to eq docs.map(&:title)
+      end
+    end
+
+    within "[data-testid=docs-list]" do
+      expected_docs.each do |org, docs|
+        docs.each do |doc|
+          expect(page).to have_link doc.title
+        end
+      end
     end
   end
 end
--- spec/features/guides/index_page_spec.rb
+++ spec/features/guides/index_page_spec.rb
@@ -1,37 +1,33 @@
 # frozen_string_literal: true
 
 RSpec.feature "Guides / Index page" do
+  let(:guide_repo) { Site::App["repos.guide_repo"] }
+
   it "lists all the guides and versions across orgs" do
     visit "/guides"
 
-    within "[data-testid=hanami-guides]" do
-      expect(page.find_all("li a").map(&:text)[0..2]).to eq [
-        "Getting started",
-        "Command line",
-        "App"
-      ]
+    expected_guides = guide_repo.latest_by_org
+
+    expected_guides.each do |org, guides|
+      within "[data-testid=#{org}-guides]" do
+        guide_titles = page.find_all("a h3").map(&:text)
+        expect(guide_titles).to eq guides.map(&:title)
+      end
     end
 
-    within "[data-testid=hanami-versions]" do
-      expect(page).to have_link "v2.1"
-      expect(page).to have_link "v2.0"
-      expect(page).to have_link "v2.2"
-
-      expect(page.find_link("v2.1")[:href]).to eq "/guides/hanami/v2.1"
+    expected_versions = guide_repo.versions_by_org
+    expected_versions.each do |org, versions|
+      within "[data-testid=#{org}-versions]" do
+        expect(page).to have_content versions.first
+      end
     end
 
-    within "[data-testid=dry-guides]" do
-      expect(page.find_all("li a").map(&:text)[0..2]).to eq [
-        "Getting started"
-      ]
-    end
-
-    within "[data-testid=rom-guides]" do
-      expect(page.find_all("li a").map(&:text)[0..2]).to eq [
-        "Getting started",
-        "Core concepts",
-        "Repositories"
-      ]
+    within "[data-testid=guides-list]" do
+      expected_guides.each do |org, guides|
+        guides.each do |guide|
+          expect(page).to have_link guide.title
+        end
+      end
     end
   end
 end
