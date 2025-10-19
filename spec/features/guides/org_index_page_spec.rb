75191a29c1afe26b61bea8abc6624a391bcf38c2
Philip Arndt <git@p.arndt.io>
2025-10-19 14:21:48 +1300

    Make the guides and docs index pages match the show pages (#122)
    I wanted to improve the look and feel of the guides and docs index pages
    because they are currently just a plain list, so I've switched to a card
    style and matched the colour schemes used elsewhere.
    
    This isn't intended to be the final result but a big step in a nicer
    direction.
    
    ### Screenshots
    
    <img width="1582" height="1034" alt="image"
    src="https://github.com/user-attachments/assets/fb84d980-d361-4e61-879f-e588a5ad2178"
    />
    <img width="1582" height="1034" alt="image"
    src="https://github.com/user-attachments/assets/04c2759d-0e07-43e9-a7a7-0d4bb404a7e4"
    />
    <img width="1582" height="1034" alt="image"
    src="https://github.com/user-attachments/assets/e1806c7a-38dd-4d2b-9601-4d1f02b6b78b"
    />
    
    
    Resolves #54
    
    Co-authored-by: Josephine Hall <josephinehall@gmail.com>

--- spec/features/guides/org_index_page_spec.rb
+++ spec/features/guides/org_index_page_spec.rb
@@ -1,27 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.feature "Guides / Org index page" do
+  let(:guide_repo) { Site::App["repos.guide_repo"] }
+
   it "renders the guides for an org at a specific version, with links to the other versions" do
-    visit "/guides/hanami/v2.0"
+    org = "hanami"
+    version = "v2.0"
+    visit "/guides/#{org}/#{version}"
 
-    within "[data-testid=versions]" do
-      expect(page).to have_link "v2.1"
-      expect(page).to have_link "v2.0"
-      expect(page).to have_link "v2.2"
-
-      expect(page.find_link("v2.1")[:href]).to eq "/guides/hanami/v2.1"
-    end
+    expected_guides = guide_repo.all_for(org:, version:)
 
     within "[data-testid=guides]" do
-      guide_links = page.find_all("li a")
+      guide_titles = page.find_all("a h3").map(&:text)
+      expect(guide_titles).to eq expected_guides.map(&:title)
 
-      expect(guide_links[0..2].map(&:text)).to eq [
-        "Getting started",
-        "Command line",
-        "App"
-      ]
-
-      expect(guide_links.first[:href]).to eq "/guides/hanami/v2.0/getting-started"
+      first_guide = expected_guides.first
+      first_card = page.find_all("a").first
+      expect(first_card[:href]).to eq first_guide.url_path
     end
   end
 end
