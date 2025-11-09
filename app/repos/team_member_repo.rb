6113627dc9bfdca9d509a8f0ac4c012a11ff75ff
Max Wheeler <max@makenosound.com>
2025-11-09 22:35:16 +1100

    Adjust blog show layout

--- app/repos/team_member_repo.rb
+++ app/repos/team_member_repo.rb
@@ -6,6 +6,10 @@ module Site
       def all_for(team:)
         team_members.where(team:).to_a
       end
+
+      def find_by_name(name)
+        team_members.where(name:).first
+      end
     end
   end
 end
--- app/views/blog/show.rb
+++ app/views/blog/show.rb
@@ -4,10 +4,19 @@ module Site
   module Views
     module Blog
       class Show < Site::View
-        include Deps["repos.post_repo"]
+        include Deps["repos.post_repo", "repos.team_member_repo"]
 
         expose :post do |permalink:|
           post_repo.get(permalink)
+        end
+
+        expose :author_team_member do |post|
+          team_member_repo.find_by_name(post.author) if post.author
+        end
+
+        # TODO: How do we get the global view to know about this value?
+        expose :org, decorate: false do |post|
+          post.org
         end
       end
     end
