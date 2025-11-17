4da0a0484ef09dbdb4dd3cf1fc9ff20413be9af6
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-11-17 08:29:41 +0100

    Move posts from Hanami blog from 2025
    * Copy content from Hanami blog
    * Change absolute URLs leading to other posts to relative ones with
      correct structure (no trailing slash)
    * Add file middleware handler for images in blog posts
    * Assign org property where needed

--- spec/requests/static_content_files_spec.rb
+++ spec/requests/static_content_files_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe "static content files", type: :request do
     expect(response.headers["ETag"].to_s).not_to be_empty
     expect(response.headers["Cache-Control"]).to eq("public, max-age=31536000, must-revalidate")
   end
+
+  it "serves static files within the blog post" do
+    response = get "/blog/2025/06/26/meet-tim-and-sean/sean-and-tim.jpeg"
+
+    expect(response.status).to eq(200)
+    expect(response.headers["Content-Type"]).to eq("image/jpeg")
+    expect(response.headers["ETag"].to_s).not_to be_empty
+    expect(response.headers["Cache-Control"]).to eq("public, max-age=31536000, must-revalidate")
+  end
 end
