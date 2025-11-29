cfd3bf8364611435b8b38f64e4c06df171ca310a
Tim Riley <tim@riley.id.au>
2025-11-29 16:30:30 +1100

    Set consistent names for first Hanami guide
    Name the guide “Getting Started”. Name the index page “Overview”.

--- content/guides/hanami/v2.2/getting-started/_index.md
+++ content/guides/hanami/v2.2/getting-started/_index.md
@@ -1,5 +1,5 @@
 ---
-title: Getting Started
+title: Overview
 pages:
   - building-a-web-app
   - building-an-api
--- spec/features/guides/redirects_spec.rb
+++ spec/features/guides/redirects_spec.rb
@@ -2,16 +2,16 @@
 
 RSpec.feature "Guides / Redirects" do
   it "redirects versionless (root) guide URLs to the default version" do
-    visit "/learn/hanami/introduction"
+    visit "/learn/hanami/getting-started"
 
     redirected_path = URI(current_url).path
-    expect(redirected_path).to eq "/learn/hanami/v2.3/introduction"
+    expect(redirected_path).to eq "/learn/hanami/v2.3/getting-started"
   end
 
   it "redirects versionless (deep) guide URLs to the default version" do
-    visit "/learn/hanami/introduction/building-a-web-app"
+    visit "/learn/hanami/getting-started/building-a-web-app"
 
     redirected_path = URI(current_url).path
-    expect(redirected_path).to eq "/learn/hanami/v2.3/introduction/building-a-web-app"
+    expect(redirected_path).to eq "/learn/hanami/v2.3/getting-started/building-a-web-app"
   end
 end
