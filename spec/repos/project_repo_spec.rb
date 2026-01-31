e5b6c79b1ee91287601a8fdb001565a3d1183cb2
Tim Riley <tim@riley.id.au>
2026-01-31 21:54:36 +1100

    Tidy

--- spec/repos/project_repo_spec.rb
+++ spec/repos/project_repo_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe Site::Repos::ProjectRepo do
       expect(repo.dry_projects.map(&:repo)).to include "dry-rb/dry-inflector"
     end
   end
+
   describe "#hanami_projects" do
     it "includes hanami repos" do
       repo = described_class.new
       expect(repo.hanami_projects.map(&:repo)).to include "hanami/hanami-cli"
     end
   end
+
   describe "#rom_projects" do
     it "includes rom repos" do
       repo = described_class.new
