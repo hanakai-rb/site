f7ef16d355d56cf31485f0324329bdec68d5376e
Max Wheeler <max@makenosound.com>
2026-03-31 09:19:48 +1100

    Sponsor page layout (#294)
    Build out fancified version of the Sponsors page.

--- app/views/pages/sponsor.rb
+++ app/views/pages/sponsor.rb
@@ -4,6 +4,29 @@ module Site
   module Views
     module Pages
       class Sponsor < Site::View
+        expose :individual_sponsors do
+          [
+            {login: "wout", avatar_id: 107324},
+            {private: true},
+            {login: "caius", avatar_id: 696},
+            {login: "CG3-Media", avatar_id: 188250871},
+            {login: "afomera", avatar_id: 1741179},
+            {login: "memoht", avatar_id: 492691},
+            {login: "danhealy", avatar_id: 38642},
+            {login: "bjeanes", avatar_id: 2560},
+            {login: "aaronmallen", avatar_id: 931094},
+            {login: "rosa", avatar_id: 813033},
+            {login: "josephinehall", avatar_id: 1491374},
+            {login: "tombruijn", avatar_id: 282402},
+            {login: "whysthatso", avatar_id: 1685114},
+            {login: "mathewdbutton", avatar_id: 1100707},
+            {login: "hedselu", avatar_id: 13008532},
+            {login: "jaredsmithse", avatar_id: 2102795},
+            {login: "schlick", avatar_id: 18456},
+            {login: "practical-computer", avatar_id: 128624803},
+            {login: "regedarek", avatar_id: 656326}
+          ]
+        end
       end
     end
   end
