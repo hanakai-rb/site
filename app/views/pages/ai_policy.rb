57fc7998c92d57e0745f49e93f7d10bc08bb2d71
Max Wheeler <max@makenosound.com>
2026-03-31 22:17:35 +1100

    Rough out shared page layout

--- app/views/pages/ai_policy.rb
+++ app/views/pages/ai_policy.rb
@@ -4,6 +4,14 @@ module Site
   module Views
     module Pages
       class AIPolicy < Site::View
+        expose :page do
+          Content::Page.new(
+            url_base: "",
+            url_path: "/ai-policy",
+            front_matter: {title: "AI contribution policy"},
+            content: Content::PAGES_PATH.join("ai_policy.md").read
+          )
+        end
       end
     end
   end
--- app/views/pages/conduct.rb
+++ app/views/pages/conduct.rb
@@ -4,6 +4,14 @@ module Site
   module Views
     module Pages
       class Conduct < Site::View
+        expose :page do
+          Content::Page.new(
+            url_base: "",
+            url_path: "/conduct",
+            front_matter: {title: "Contributor Covenant Code of Conduct"},
+            content: Content::PAGES_PATH.join("conduct.md").read
+          )
+        end
       end
     end
   end
